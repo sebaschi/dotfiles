f03ce1b9b05e2410bddbe5b4581780e5e9ac92c2
Sebastian Lenzlinger <74497638+sebaschi@users.noreply.github.com>
2024-05-12 11:42:53 +0200

    Remove scripts from repo and rename rsync config folder.

--- bash/scripts/scripts/db23-arrange.sh
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/bash
-filename=$1
-arrangedFolder=${PWD}/arranged
-newFilename=${filename:0:3}db23-arranged.pdf
-echo "Arranging $filename"
-/usr/bin/pdfjam --nup 2x2 --landscape --outfile $newFilename $filename
-echo "Moving $new-filename to $arranged-folder"
-mv $newFilename $arrangedFolder
-echo "Done"
